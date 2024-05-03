9f9b25e734642579e7ab455fbaaa245b00bcf856
oolongtarou <oolongtarou@gmail.com>
2024-05-03 22:14:31 +0900

    update: 単語カードに対してホバーイベントを追加した。

--- react-app/src/domains/WordCard/WordCard.tsx
+++ react-app/src/domains/WordCard/WordCard.tsx
@@ -2,11 +2,13 @@ import MonthChips from './MonthChips';
 import WordTags from './WordTags';
 import Description from './WordDescription';
 import WordTitle from './WordTitle';
+import './WordCard.css'
+
 
 export default function WordCard() {
   return (
-    <div style={{display:'flex', flexWrap:'nowrap', maxHeight:'300px'}}>
-      <div style={{width:'15%', marginTop:'5px', marginBottom:'5px'}}>
+    <div style={{display:'flex', flexWrap:'nowrap', maxHeight:'300px'}} className='word-card'>
+      <div style={{width:'15%', marginTop:'5px', marginBottom:'5px', paddingLeft:'5px'}}>
               <MonthChips />          
       </div>
       <div style={{width:'85%', marginLeft:'10px'}}>
