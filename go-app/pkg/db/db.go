d9666cc1f7567e9d41f81cf5c5f757d953bf530c
oolongtarou <oolongtarou@gmail.com>
2024-05-06 04:12:08 +0900

    update: クラウド上でDBに接続できるかどうかのテスト。

--- go-app/pkg/db/db.go
+++ go-app/pkg/db/db.go
@@ -14,12 +14,13 @@ func Connect() (*gorm.DB, error) {
 	// db_name := os.Getenv("MYSQL_DATABASE")
 	db_name := "tea_ins"
 	conn_name := os.Getenv("MYSQL_CONN_NAME")
-	var path string = fmt.Sprintf("%s:%s@unix(%s)/%s?parseTime=true", user, pw, conn_name, db_name)
+	var path string = fmt.Sprintf("%s:%s@unix(/cloudsql/%s)/%s", user, pw, conn_name, db_name)
+	// var path string = fmt.Sprintf("%s:%s@unix(%s)/%s?parseTime=true", user, pw, conn_name, db_name)
 	// var path string = fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8&parseTime=true", user, pw, conn_name, db_name)
 
 	db, err := gorm.Open(mysql.Open(path), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Db open error:", err.Error())
+		// log.Fatal("Db open error:", err.Error())
 		return db, err
 	}
 
