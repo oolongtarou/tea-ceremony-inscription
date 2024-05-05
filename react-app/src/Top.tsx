91f96f3465281e716ebdfe1c0040c124b5daaf54
oolongtarou <oolongtarou@gmail.com>
2024-05-05 13:50:54 +0900

    update: スタート画面を用意した。

--- react-app/src/Top.tsx
+++ react-app/src/Top.tsx
@@ -1,13 +1,18 @@
 import { Routes, Route, Link } from "react-router-dom";
 
-import Newtop from './New'
-
 function Top() {
     return (
         <div className="l-full">
             <div className="c-box l-full__content">
-                <h1>content</h1>
-                <Link to='/App'>ログインせずにスタート</Link>
+                <h1>茶の湯の銘 図鑑</h1>
+                <Link to='/App'
+                    style={{
+                        fontSize:'3em',
+                        color:'white'
+                    }}
+                >
+                    スタート
+                    </Link>
             </div>
         </div>
     )
