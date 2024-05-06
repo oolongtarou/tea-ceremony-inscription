4f01fce8199dc7a697268ebbb0f57944753a0588
oolongtarou <oolongtarou@gmail.com>
2024-05-07 00:16:31 +0900

    update: デプロイするときの情報をコミットする。

--- react-app/src/main.tsx
+++ react-app/src/main.tsx
@@ -6,6 +6,29 @@ import { BrowserRouter, RouterProvider, createBrowserRouter } from 'react-router
 import Newtop from './New.tsx'
 import Top from './Top.tsx'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCoGxlCRmE-99kmT4iuFVnkGHGJQwMsPIw",
+  authDomain: "teaceremonyreact.firebaseapp.com",
+  projectId: "teaceremonyreact",
+  storageBucket: "teaceremonyreact.appspot.com",
+  messagingSenderId: "501080584905",
+  appId: "1:501080584905:web:566845583f78fda0781318",
+  measurementId: "G-PN4LCBWZKN"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+console.log(analytics)
+
 
 const router = createBrowserRouter([
   {
