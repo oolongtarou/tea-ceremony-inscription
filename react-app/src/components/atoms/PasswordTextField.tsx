3f3f15d0a68396ebd61a8cce780f3ad98e9c7a67
oolongtarou <oolongtarou@gmail.com>
2024-05-18 00:02:34 +0900

    feat: パスワード入力用のテキストボックスを追加した。

---
+++ react-app/src/components/atoms/PasswordTextField.tsx
@@ -0,0 +1,41 @@
+import {  IconButton, InputAdornment, TextField} from "@mui/material";
+
+import { UseFormRegisterReturn } from "react-hook-form"
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
+import { useState } from "react";
+
+
+interface PasswordFieldProps {
+    formRegtiser: UseFormRegisterReturn<string>
+}
+
+export default function PasswordTextField(props:PasswordFieldProps) {
+    const [showPassword, setShowPassword] = useState(false)
+    function handleClickShowPassword() {
+        setShowPassword(!(showPassword))
+    }
+    
+    return (
+        <TextField 
+        type={showPassword ? 'text' : 'password'}
+        label="パスワード" 
+        variant="filled" 
+        fullWidth 
+        InputProps={{
+            endAdornment: 
+                <InputAdornment position="end">
+                    <IconButton
+                        aria-label="toggle password visibility"
+                        onClick={handleClickShowPassword}
+                        edge="end"
+                    >
+                    {showPassword ? <VisibilityOffIcon /> : <VisibilityIcon />}
+                    </IconButton>
+                </InputAdornment>,
+        }} 
+        required 
+        {...props.formRegtiser}
+        />
+    )
+}
