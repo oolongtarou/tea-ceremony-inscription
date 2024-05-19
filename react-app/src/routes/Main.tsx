33203347ab9695ac16aeaddcd88149ace0612ef7
oolongtarou <oolongtarou@gmail.com>
2024-05-19 23:11:05 +0900

    update: firebaseの設定情報を外部ファイル参照に変更した。

--- react-app/src/routes/Main.tsx
+++ react-app/src/routes/Main.tsx
@@ -7,20 +7,19 @@ import Top from './Top.tsx';
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCoGxlCRmE-99kmT4iuFVnkGHGJQwMsPIw",
-  authDomain: "teaceremonyreact.firebaseapp.com",
-  projectId: "teaceremonyreact",
-  storageBucket: "teaceremonyreact.appspot.com",
-  messagingSenderId: "501080584905",
-  appId: "1:501080584905:web:566845583f78fda0781318",
-  measurementId: "G-PN4LCBWZKN"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID 
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
--- react-app/src/vite-env.d.ts
+++ react-app/src/vite-env.d.ts
@@ -2,6 +2,13 @@
 
 interface ImportMetaEnv {
     readonly VITE_DOMAIN: string
+    readonly VITE_API_KEY :string
+    readonly VITE_AUTH_DOMAIN :string
+    readonly VITE_PROJECT_ID :string
+    readonly VITE_STORAGE_BUCKET :string
+    readonly VITE_MESSAGING_SENDER_ID :string
+    readonly VITE_APP_ID :string
+    readonly VITE_MEASUREMENT_ID :string
   }
   
   interface ImportMeta {
