dc0a251936a498f238666437e8bbce0437ee0701
oolongtarou <oolongtarou@gmail.com>
2024-05-19 20:21:41 +0900

    update: アカウント作成時にメールアドレスとパスワードを検証する機能を追加した。

--- react-app/src/components/organisms/Signup.tsx
+++ react-app/src/components/organisms/Signup.tsx
@@ -17,6 +17,9 @@ type SignupProps = {
     toggleLoginSignup: () => void
 }
 
+const mailAddressRegex = /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/g
+const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d-_]{14,32}$/g;
+
 const Signup: React.FC<SignupProps> = (props) => {
   const navigate = useNavigate();
   const { register, handleSubmit } = useForm<IFormInput>()
@@ -25,9 +28,32 @@ const Signup: React.FC<SignupProps> = (props) => {
   const [mailAddressError, setMailAddressError] = useState(false)
   const [mailAddressHelperText, setMailAddressHelperText] = useState("")
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
-    const inputData = new FormData();
-    inputData.append("mailAddress", data.mailAddress)
-    inputData.append("password", data.password)
+    const inputData = new FormData()
+    const mailAddress = data.mailAddress
+    const password = data.password
+    inputData.append("mailAddress", mailAddress)
+    inputData.append("password", password)
+
+    if (!isValidMailAddress(mailAddress)) {
+      setMailAddressError(true)
+      setMailAddressHelperText("有効なメールアドレスではありません。")
+      return
+    }
+    else {
+      setMailAddressError(false)
+      setMailAddressHelperText("")
+    }
+
+    if(!isValidPassword(password)){
+      setPasswordError(true)
+      setPasswordHelperText("有効なパスワードではありません。")
+      return
+    }
+    else{
+      setPasswordError(false)
+      setPasswordHelperText("")
+    }
+
     axios.post(`${import.meta.env.VITE_DOMAIN}/signup`, inputData, {withCredentials: true})
       .then((response) => {
         try{
@@ -105,9 +131,9 @@ const Signup: React.FC<SignupProps> = (props) => {
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Card sx={{ p: 4, width: "400px", minHeight: "400px", m: "20px auto" }}>
-          <MailAddressTextField error={false} helperText="" formRegtiser={register("mailAddress")} />
-          <PasswordTextField error={false} helperText="" formRegtiser={register("password")} />
-          <Typography display="flex" justifyContent="flex-end" sx={{color:"#909090"}}>※ 半角英数字8字以上</Typography>
+          <MailAddressTextField error={mailAddressError} helperText={mailAddressHelperText} formRegtiser={register("mailAddress")} />
+          <PasswordTextField error={passwordError} helperText={passwordHelperText} formRegtiser={register("password")} />
+          <Typography display="flex" justifyContent="flex-end" sx={{color:"#909090"}}>※ 半角英数字12字以上32字以内</Typography>
           <Box mt={3}>
             <Button type="submit" color="primary" variant="contained" fullWidth sx={{fontSize: "1.1rem" }}>
               登録してログイン
@@ -123,3 +149,15 @@ const Signup: React.FC<SignupProps> = (props) => {
 
 
 export default Signup
+
+
+
+function isValidMailAddress(mailAddress: string): boolean {
+  const result = mailAddressRegex.test(mailAddress)
+  return result
+}
+
+function isValidPassword(password: string): boolean {
+  const result = passwordRegex.test(password)
+  return result
+}
